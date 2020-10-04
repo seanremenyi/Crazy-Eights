8fa8ba26603a5e5c483ac5fcd7034a7d87c4b154
Sean <sean.remenyi@gmail.com>
2020-10-04 01:34:01 +0000

    card dealt

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 class Deck():
     cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"]
     suits = ["♠", "♥", "♣", "♦"]
@@ -9,12 +11,22 @@ class Deck():
         for value in cls.cards:
             for suit in cls.suits:
                 cls.deck.append(value+suit)
+                
+    @classmethod
+    def deal_card(cls):
+        card = random.choice(cls.deck)
+        cls.deck.pop(cls.deck.index(card))
+        return card
 
-
-class Hand()
+class Hand(Deck):
+    def __init__(self):
+        self.card= Deck.deal_card()
+    
 
 
 
 new=Deck()
 new.create_deck()
-print(new.deck)
+new1=Hand()
+print(new1.card)
+print(new1.deck)
